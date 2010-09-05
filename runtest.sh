28708c7e5ef32668679e2c7a8f772034fb05aec7
Maksim Sipos <max@planck.(none)>
2010-09-04 19:50:07 -0500

    Made tests much less verbose.

--- runtest.sh
+++ runtest.sh
@@ -1,36 +1,12 @@
 #!/bin/bash
 
-FLAGS="-m Test -m Gsl -m Sdl -m Math -m TextFile -m Gd"
+FLAGS="-m Test -m Gsl -m Sdl -m Math -m Gd"
 BIN="product/ripe"
 
-FILE=operators.rip
+FILE=language.rip
 echo "Running $FILE ..."
 $BIN $FLAGS test/suite/$FILE
 
 FILE=stdlib.rip
-echo "Running $FILE ..."
-$BIN $FLAGS test/suite/$FILE
-
-FILE=varargs.rip
-echo "Running $FILE ..."
-$BIN $FLAGS test/suite/$FILE
-
-FILE=basic-class.rip
-echo "Running $FILE ..."
-$BIN $FLAGS test/suite/$FILE
-
-FILE=c-expr.rip
-echo "Running $FILE ..."
-$BIN $FLAGS test/suite/$FILE
-
-FILE=cdata-class.rip
-echo "Running $FILE ..."
-$BIN $FLAGS test/suite/$FILE
-
-FILE=sdl.rip
-echo "Running $FILE ..."
-$BIN $FLAGS test/suite/$FILE
-
-FILE=draw.rip
 echo "Running $FILE ..."
 $BIN $FLAGS test/suite/$FILE
